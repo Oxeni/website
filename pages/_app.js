18def25439de9af43aee7aed88575ada5a9182e3
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-01-04 22:54:40 +0400

    :rocket: added the og images

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/main.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="title" content="4dao" />
+        <meta name="description" content="cutting edge technology to create a truly decentralized and resilient internet of the future" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://oxeni.dev/meta_images/og_image.png" />
+        <meta property="og:title" content="4dao" />
+        <meta property="og:description" content="cutting edge technology to create a truly decentralized and resilient internet of the future" />
+        <meta property="og:image" content="/meta_images/og_image.png" />/
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://oxeni.dev/meta_images/og_image.png" />
+        <meta property="twitter:title" content="4dao" />
+        <meta property="twitter:description" content="cutting edge technology to create a truly decentralized and resilient internet of the future" />
+        <meta property="twitter:image" content="/meta_images/og_image.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
