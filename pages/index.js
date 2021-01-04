196bbda9ec0f979883c2a99d6989ea3b7cc05cd0
Klaatu98 <arabson1998@gmail.com>
2021-01-04 22:31:11 +0400

    :beer: about

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <>
-        <div className="" style={{width:"100%",heigth:"100vh"}}>
-            <img style={{width:"100%",heigth:"100vh"}} src="/1.svg" alt="oxeni"/>
+        <div className="" style={{width:"100%",height:"100vh"}}>
+            <img style={{width:"100%",height:"100vh"}} src="/1.svg" alt="oxeni"/>
         </div>
     </>
   )
