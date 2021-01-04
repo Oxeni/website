fdd474fbd62f04cb59fc500e7d41d116f43c91cb
Klaatu98 <arabson1998@gmail.com>
2021-01-04 22:14:38 +0400

    :beer: add pic

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <>
-        <h1>Oxeni</h1>
+        <div className="" style={{width:"100%",heigth:"100vh"}}>
+            <img style={{width:"100%",heigth:"100vh"}} src="/1.svg" alt="oxeni"/>
+        </div>
     </>
   )
 }
