c6cfcdc4672600791faa0e5aa51349ce1410bbd5
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-01-04 22:57:55 +0400

    :rocket: edited the og info

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="title" content="4dao" />
-        <meta name="description" content="cutting edge technology to create a truly decentralized and resilient internet of the future" />
+        <meta name="title" content="🔵 Oxeni" />
+        <meta name="description" content="cutting edge technology to create true technical beauty of the future" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://oxeni.dev/meta_images/og_image.png" />
-        <meta property="og:title" content="4dao" />
-        <meta property="og:description" content="cutting edge technology to create a truly decentralized and resilient internet of the future" />
+        <meta property="og:title" content="🔵 Oxeni" />
+        <meta property="og:description" content="cutting edge technology to create true technical beauty of the future" />
         <meta property="og:image" content="/meta_images/og_image.png" />/
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://oxeni.dev/meta_images/og_image.png" />
-        <meta property="twitter:title" content="4dao" />
-        <meta property="twitter:description" content="cutting edge technology to create a truly decentralized and resilient internet of the future" />
+        <meta property="twitter:title" content="🔵 Oxeni" />
+        <meta property="twitter:description" content="cutting edge technology to create true technical beauty of the future" />
         <meta property="twitter:image" content="/meta_images/og_image.png" />
       </Head>
 
