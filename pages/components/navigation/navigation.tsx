325366db52c60f90ba340628aa4c03cb6e02f87d
Klaatu98 <arabson1998@gmail.com>
2021-01-09 17:11:35 +0400

    removed cube css

--- pages/components/navigation/navigation.tsx
+++ pages/components/navigation/navigation.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 const navigation = () => {
     return (
         <>
-            <div className="[ navigation ]">
-                <div className="[ navigation_container ]">
-                    <div className="[ logo ]"></div>
+            <div className="navigation">
+                <div className="navigation_container">
+                    <div className="logo"></div>
                 </div>
             </div>   
         </>
