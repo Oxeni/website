f31f012adbbdc72d352b5ac28487df79c73d6160
Klaatu98 <arabson1998@gmail.com>
2021-01-09 14:05:51 +0400

    keep app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,32 @@
+import Head from "next/head";
 import "../styles/main.scss";
 import Navigation from "pages/components/navigation/navigation";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navigation/>
+      <Head>
+        <meta name="title" content="🔵 Oxeni" />
+        <meta name="description" content="cutting edge technology to create true technical beauty of the future"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://oxeni.dev/meta_images/og_image.png"
+        />
+        <meta property="og:title" content="🔵 Oxeni" />
+        <meta property="og:description" content="cutting edge technology to create true technical beauty of the future"
+        />
+        <meta property="og:image" content="/meta_images/og_image.png" />/
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://oxeni.dev/meta_images/og_image.png"
+        />
+        <meta property="twitter:title" content="🔵 Oxeni" />
+        <meta property="twitter:description" content="cutting edge technology to create true technical beauty of the future"
+        />
+        <meta property="twitter:image" content="/meta_images/og_image.png" />
+      </Head>
+
+
+      <Navigation />
       <Component {...pageProps} />
     </>
   );
