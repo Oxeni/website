e9091640b799c3b397d6d8401cc5c99106059211
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-01-16 15:26:42 +0400

    :rocket: fixed and changed the font

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,13 @@ const index = () => {
         <div className="landing_container">
           <div className="contact_message">
             <div className="heading">
-              <p className="black f-size-h1">
+              <p className="f-weight-bl f-size-h1">
                 let's make your business the best in the biz
               </p>
             </div>
             <div className="line"></div>
             <div className="paragraph">
-              <p className="regular f-size-p1">
+              <p className="f-weight-l f-size-p1">
                 <strong>Oxeni</strong> is a web development micro business. We want to start a business
                 and create something unique The face of the brand and the site
                 with which you will be able to get more customers Attraction.
