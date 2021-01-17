e2f5528f679ad8992699e43f7851238c46b23079
Klaatu98 <arabson1998@gmail.com>
2021-01-17 11:41:33 +0400

    :beer: add mobile

---
+++ pages/components/index_components/contact-proposal.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function contactProposal() {
+    return (
+        <>
+        <div className="mobile_contact_message">
+            <div className="heading">
+              <p className="f-weight-bl f-size-h1">
+              let's make your business the best in the biz
+              </p>
+            </div>
+            <div className="paragraph">
+              <p className="f-weight-r f-size-p1">
+                <strong style={{fontWeight:600}}>Oxeni</strong> is a web development micro business. We want to start a business
+                  and create something unique The face of the brand and the site
+                  with which you will be able to get more customers Attraction.
+              </p>
+            </div>
+          </div>
+        </>
+    )
+}
---
+++ pages/components/index_components/landing.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function landing() {
+    return (
+        <>
+        <div className="landing">
+          <div className="landing_container">
+            <div className="contact_message">
+              <div className="heading">
+                <p className="f-weight-bl f-size-h1">
+                  let's make your business the best in the biz
+                </p>
+              </div>
+              <div className="line"></div>
+              <div className="paragraph">
+                <p className="f-weight-l f-size-p1">
+                  <strong>Oxeni</strong> is a web development micro business. We want to start a business
+                  and create something unique The face of the brand and the site
+                  with which you will be able to get more customers Attraction.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+}
