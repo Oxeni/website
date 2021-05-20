77ae24dc6b6f86b820f8bce2a563f6e6a99b4298
Klaatu98 <arabson1998@gmail.com>
2021-05-20 21:15:41 +0400

    add fix width

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+
+import dynamic from 'next/dynamic';
 
 import Landing from "components/pages/index/hero/Hero";
-import Projects from "components/pages/index/projects/Project";
+// import Projects from "components/pages/index/projects/Project";
+
+const Projects = dynamic(() => import("components/pages/index/projects/Project"),{ssr:false})
 import Loading from "components/global/loading/Loading";
 import Other_works from "components/pages/index/other_works/Other_works";
 import Services from "components/pages/index/services/service";
