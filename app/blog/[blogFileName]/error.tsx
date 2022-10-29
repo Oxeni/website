ee95a94e6f494652a68ae557998c6ef7863f7e74
Robinzon100 <robinzon.sokhashvili@gmail.com>
2022-10-29 23:26:35 +0400

    :star2: testing blogpost solution

--- app/blog/[blogFileName]/error.tsx
+++ app/blog/[blogFileName]/error.tsx
@@ -1,8 +1,26 @@
+'use client';
 
-const BlogPostError = () => {
-  return (
-    <h1>error</h1>
-  )
+import { useEffect } from 'react';
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div>
+            <p>Something went wrong!</p>
+            <pre>
+                {JSON.stringify(error, null, 4)}
+            </pre>
+            <button onClick={() => reset()}>Reset error boundary</button>
+        </div>
+    );
 }
-
-export default BlogPostError
